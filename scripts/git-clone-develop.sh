eb0ad81412a22c1617d9a02601b35599c5cf5a51
Liviu Ionescu <ilg@livius.net>
2021-09-24 22:27:07 +0300

    remove git-clone scripts

--- scripts/git-clone-develop.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-rm -rf "${HOME}/Downloads/cmake-xpack.git"
-git clone \
-  --recurse-submodules \
-  --branch xpack-develop \
-  https://github.com/xpack-dev-tools/cmake-xpack.git \
-  "${HOME}/Downloads/cmake-xpack.git"
--- scripts/git-clone.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-rm -rf "${HOME}/Downloads/cmake-xpack.git"
-git clone \
-  --recurse-submodules \
-  https://github.com/xpack-dev-tools/cmake-xpack.git \
-  "${HOME}/Downloads/cmake-xpack.git"
