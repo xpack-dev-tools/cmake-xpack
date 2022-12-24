765c0e57babf193d8413f245c958cb8575f1aa04
Liviu Ionescu <ilg@livius.net>
2022-12-24 16:41:20 +0200

    versioning.sh: updates

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -7,26 +7,10 @@
 # for any purpose is hereby granted, under the terms of the MIT license.
 # -----------------------------------------------------------------------------
 
-# Helper script used in the xPack build scripts. As the name implies,
-# it should contain only functions and should be included with 'source'
-# by the build scripts (both native and container).
-
 # -----------------------------------------------------------------------------
 
 function application_build_versioned_components()
 {
-  # Don't use a comma since the regular expression
-  # that processes this string in the Makefile, silently fails and the
-  # bfdver.h file remains empty.
-  export XBB_BRANDING="${XBB_APPLICATION_DISTRO_NAME} ${XBB_APPLICATION_NAME} ${XBB_REQUESTED_TARGET_MACHINE}"
-
-  # cmake_BUILD_GIT_BRANCH=${cmake_BUILD_GIT_BRANCH:-"master"}
-  # cmake_BUILD_GIT_COMMIT=${cmake_BUILD_GIT_COMMIT:-"HEAD"}
-
-  # Use this for custom content, otherwise the generic README-OUT.md
-  # will be copied to the archive.
-  # README_OUT_FILE_NAME=${README_OUT_FILE_NAME:-"README-${XBB_RELEASE_VERSION}.md"}
-
   export XBB_CMAKE_VERSION="$(echo "${XBB_RELEASE_VERSION}" | sed -e 's|-.*||')"
 
   # When 3.x.3 is out, release 3.x-1.y
@@ -34,9 +18,18 @@ function application_build_versioned_components()
   if [[ "${XBB_RELEASE_VERSION}" =~ 3\.22\.* ]]
   then
     # -------------------------------------------------------------------------
+    # Build the native dependencies.
 
-    xbb_set_binaries_install "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
-    xbb_set_libraries_install "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
+    # None
+
+    # -------------------------------------------------------------------------
+    # Build the target dependencies.
+
+    xbb_reset_env
+    xbb_set_target "requested"
+
+    xbb_set_executables_install_path "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
+    xbb_set_libraries_install_path "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
 
     # http://zlib.net/fossils/
     zlib_build "1.2.12"
@@ -54,13 +47,16 @@ function application_build_versioned_components()
     # https://www.openssl.org/source/old/
     openssl_build "1.1.1q"
 
+    # -------------------------------------------------------------------------
+    # Application binaries.
 
-    xbb_set_binaries_install "${XBB_APPLICATION_INSTALL_FOLDER_PATH}"
+    xbb_set_executables_install_path "${XBB_APPLICATION_INSTALL_FOLDER_PATH}"
+    xbb_set_libraries_install_path "${XBB_DEPENDENCIES_INSTALL_FOLDER_PATH}"
 
     cmake_build "${XBB_CMAKE_VERSION}"
 
   else
-    echo "Unsupported version ${XBB_RELEASE_VERSION}."
+    echo "Unsupported ${XBB_APPLICATION_LOWER_CASE_NAME} version ${XBB_RELEASE_VERSION}"
     exit 1
   fi
 }
