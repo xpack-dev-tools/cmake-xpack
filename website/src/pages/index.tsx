ce1ef14049f7e111da4c3f3323461e41a5bcc0a3
Liviu Ionescu <ilg@livius.net>
2024-10-10 01:32:40 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from xPack Binary Development Tools`}
-      description="A binary xPack with the CMake executables">
+      description="A binary package with the CMake executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
