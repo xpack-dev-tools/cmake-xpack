7dc9d37758b15901ea4728128225f13966ed0b67
Liviu Ionescu <ilg@livius.net>
2024-10-14 00:33:40 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Hello from cmake`}
+      title={`Welcome to cmake`}
       description="A binary package with the cmake executables">
       <HomepageHeader />
       <main>
