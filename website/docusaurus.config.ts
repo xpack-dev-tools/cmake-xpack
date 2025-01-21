f1b429758590ed86680072fd4d89e6f62bcd6c97
Liviu Ionescu <ilg@livius.net>
2025-01-21 12:14:05 +0200

    website: re-generate commons

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -275,7 +275,7 @@ const config: Config = {
           label: 'GitHub',
           items: [
             {
-              label: `cmake-xpack`,
+              label: `cmake-xpack project`,
               href: `https://github.com/xpack-dev-tools/cmake-xpack/`,
             },
             {
@@ -348,7 +348,7 @@ const config: Config = {
               href: 'https://www.paypal.com/donate/?hosted_button_id=5MFRG9ZRBETQ8',
             },
             {
-              label: 'GitHub cmake-xpack',
+              label: 'GitHub cmake-xpack project',
               href: 'https://github.com/xpack-dev-tools/cmake-xpack/',
             },
             {
