42d5eaf41f581b9a2bebb2be8eeb675a5be4b7cc
Liviu Ionescu <ilg@livius.net>
2025-10-19 12:00:22 +0300

    re-generate commons

--- build-assets/scripts/versioning.sh
+++ build-assets/scripts/versioning.sh
@@ -31,7 +31,7 @@ function application_build_versioned_components()
 
     xbb_set_target "requested"
 
-    # https://zlib.net/
+    # https://zlib.net/fossils/
     zlib_build "1.3.1"
 
     if [ "${XBB_REQUESTED_HOST_PLATFORM}" != "win32" ]
